0491543a0ba083720fb25d8098a2a8118a980d58
Walter Nelson <walterj.nelson@mail.utoronto.ca>
2024-08-19 10:11:45 -0400

    initialize project and fix tests

--- test/test_readapt.py
+++ test/test_readapt.py
@@ -10,8 +10,9 @@ from transformers_readapt import readapt
 
 class TestReadapt(unittest.TestCase):
     def setUp(self):
+        self.base_model = AutoModelForCausalLM.from_pretrained("afmck/testing-llama-tiny")
         self.finetuned_model = AutoModelForCausalLM.from_pretrained("afmck/testing-llama-tiny")
         self.instruction_model = AutoModelForCausalLM.from_pretrained("afmck/testing-llama-tiny")
 
     def test_readapt(self):
-        combined_model = readapt(self.finetuned_model, self.instruction_model)
+        combined_model = readapt(self.base_model, self.finetuned_model, self.instruction_model)
